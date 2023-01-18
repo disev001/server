bc1abacddc8f9008d72efb4ea07d51665549a748
Dima Severgin <git@doldrums-gc.de>
2023-01-18 17:12:23 +0100

    first commit

---
+++ setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#Check Sudo
+if [ "$(id -u)" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+echo "Root granted for $SUDO_USER. Setting up Docker for Fedora"
+#install updates & docker
+dnf update -y && dnf upgrade -y
+dnf install -y nano dnf-plugins-core
+dnf config-manager -y --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+dnf update -y
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#setup docker for user
+systemctl enable docker.service
+systemctl enable containerd.service
+groupadd docker
+usermod -aG docker $SUDO_USER
+mkdir -p /home/$SUDO_USER/.docker
+chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.docker -R
+chmod g+rwx /home/$SUDO_USER/.docker -R
+
+#install kubernetes
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+yum install -y kubectl
+systemctl start docker
+newgrp docker
+docker ps
