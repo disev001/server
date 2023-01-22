29a4926601b20f167c1ed92463f43fbc717b696a
Dima Severgin <git@doldrums-gc.de>
2023-01-22 10:59:29 +0100

    anpassungen und split für devops setup

--- setup.sh
+++ setup.sh
@@ -9,27 +9,23 @@ dnf config-manager -y --add-repo https://download.docker.com/linux/fedora/docker
 dnf update -y
 dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 #setup docker for user
-systemctl enable docker.service
-systemctl enable containerd.service
-groupadd docker
+groupadd -f docker
 usermod -aG docker $SUDO_USER
 mkdir -p /home/$SUDO_USER/.docker
 chown $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.docker -R
 chmod g+rwx /home/$SUDO_USER/.docker -R
-
-#install kubernetes
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-yum install -y kubectl
 systemctl start docker
-newgrp docker
-docker ps
-
-#aufrufbar über curl -s https://raw.githubusercontent.com/disev001/server/master/setup.sh | bash
-#oder download curl https://raw.githubusercontent.com/disev001/server/master/setup.sh >> filename.sh
+systemctl enable docker.service
+systemctl enable containerd.service
+while true; do
+    read -p "Do you wish to restart to apply the settings? " yn
+    case $yn in
+        [Yy]* ) sudo reboot; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+exit
+#aufrufbar über: curl -s https://raw.githubusercontent.com/disev001/server/master/setup.sh | bash
+#oder download: curl https://raw.githubusercontent.com/disev001/server/master/setup.sh >> filename.sh
+#Installation per lokalen ssh aufruf: curl -s https://raw.githubusercontent.com/disev001/server/master/setup.sh | ssh disev@192.168.178.43 'bash -s
